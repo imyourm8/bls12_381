dbab8fed8580ec243c7b67a2cab43b38d01633bd
CPerezz <c.perezbaro@gmail.com>
2020-04-04 15:46:00 +0200

    Add MSM benches

--- benches/groups.rs
+++ benches/groups.rs
@@ -7,6 +7,38 @@ use bls12_381::*;
 use criterion::{black_box, Criterion};
 
 fn criterion_benchmark(c: &mut Criterion) {
+    // MSM implementations
+    {
+        // Reuse points across different tests
+        let mut n = 512;
+        let x = Scalar::from(2128506u64).invert().unwrap();
+        let y = Scalar::from(4443282u64).invert().unwrap();
+        let points = (0..n)
+            .map(|i| G1Projective::generator() * Scalar::from(1 + i as u64))
+            .collect::<Vec<_>>();
+        let scalars = (0..n)
+            .map(|i| x + (Scalar::from(i as u64) * y))
+            .collect::<Vec<_>>();
+
+        let scalars_pip = scalars.to_owned().into_iter();
+        let points_pip = points.to_owned().into_iter();
+
+        let points_var_base: Vec<G1Affine> = points.to_owned().into_iter().map(|p| G1Affine::from(p)).collect();
+        let scalars_var_base = scalars.to_owned();
+
+        c.bench_function("Naive MSM", move |b| {
+            b.iter(|| multiscalar_mul::naive_msm(&points, &scalars))
+        });
+
+        c.bench_function("Pippenger", move |b| {
+            b.iter(|| multiscalar_mul::pippenger(points_pip.clone(), scalars_pip.clone()))
+        });
+
+        c.bench_function("Variable Base MSM", move |b| {
+            b.iter(|| multiscalar_mul::msm_variable_base(&points_var_base.clone(), &scalars_var_base.clone()))
+        });
+        
+    }
     // Pairings
     {
         let g = G1Affine::generator();
@@ -86,7 +118,10 @@ fn criterion_benchmark(c: &mut Criterion) {
             b.iter(|| black_box(a).add_mixed(&a_affine))
         });
         c.bench_function(&format!("{} scalar multiplication", name), move |b| {
-            b.iter(|| black_box(a) * black_box(s))
+            b.iter(|| a * s)
+        });
+        c.bench_function(&format!("{} NAF scalar multiplication", name), move |b| {
+            b.iter(|| a.binary_naf_mul(&s))
         });
         c.bench_function(&format!("{} batch to affine n={}", name, N), move |b| {
             b.iter(|| {
